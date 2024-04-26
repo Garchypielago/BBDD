21e825ed1979820b8516406711b91369393a74fc
alejandro <jandro.garcia.sanchezr@gmail.com>
2024-04-26 19:17:56 +0200

    26/04clase

--- PLSQL/EJERCICIOS_SCRIPS/HR/HR_TRIGGERS.sql
+++ PLSQL/EJERCICIOS_SCRIPS/HR/HR_TRIGGERS.sql
@@ -74,20 +74,142 @@ impedirlo dando un mensaje por consola.
     a. Por ejemplo, el salario mínimo de IT_PROG es 4.000. Si se quisiera cambiar
     el salario mínimo a 5.000, debe dar error porque hay trabajadores con este
     trabajo que no ganan 5.000.*/
+create or replace trigger T_CAMBIO_MINMAX
+    BEFORE UPDATE OF MIN_SALARY, MAX_SALARY
+    ON JOBS FOR EACH ROW
+DECLARE
+    E_COBRAMENOS EXCEPTION;
+    E_COBRAMAS EXCEPTION;
+    
+    V_CONTMIN NUMBER:=0;
+    V_CONTMAX NUMBER:=0;
+BEGIN
+    SELECT COUNT(1)
+    INTO V_CONTMIN
+        FROM EMPLOYEES EM
+        WHERE em.salary<:NEW.MIN_SALARY;
+        
+     IF V_CONTMIN > 0 THEN
+         RAISE E_COBRAMENOS;
+     END IF;
+        
+    SELECT COUNT(1)
+    INTO V_CONTMAX
+        FROM EMPLOYEES EM
+        WHERE em.salary<:NEW.MAX_SALARY;
 
+    IF V_CONTMAX > 0 THEN
+        RAISE E_COBRAMAS;
+    END IF;
+    
+EXCEPTION
+    WHEN E_COBRAMENOS THEN
+        RAISE_APPLICATION_ERROR(-20001,'UN EMPLEADO COBRA MENOS');
+        RETURN;
+    WHEN E_COBRAMAS THEN
+        RAISE_APPLICATION_ERROR(-20002,'UN EMPLEADO COBRA MAS');
+        RETURN;
+    WHEN OTHERS THEN
+        RETURN;
+END;
+
+UPDATE JOBS SET MIN_SALARY=5000 WHERE JOB_ID = 'IT_PROG';
+SELECT * FROM JOBS;
 
 
 /*4. Crea un trigger que me avise al cambiar el departamento de un empleado si el
 departamento antiguo y el nuevo no están en la misma ciudad.*/
+create or replace trigger T_CAMBIO_CIUDAD
+    AFTER UPDATE OF DEPARTMENT_ID
+    ON EMPLOYEES FOR EACH ROW
+DECLARE
+    E_DIFCIU EXCEPTION;
+    V_CIUOLD LOCATIONS.CITY%TYPE;
+    V_CIUNEW LOCATIONS.CITY%TYPE;
+BEGIN
+    SELECT LO.CITY
+    INTO V_CIUNEW
+        FROM LOCATIONS LO
+        JOIN DEPARTMENTS DE ON de.location_id=lo.location_id
+        WHERE de.department_id=:NEW.DEPARTMENT_ID;
+    
+    SELECT LO.CITY
+    INTO V_CIUOLD
+        FROM LOCATIONS LO
+        JOIN DEPARTMENTS DE ON de.location_id=lo.location_id
+        WHERE de.department_id=:OLD.DEPARTMENT_ID;
+    
+    IF V_CIUNEW != V_CIUOLD THEN
+        RAISE E_DIFCIU;
+    END IF;
+    
+EXCEPTION
+    WHEN E_DIFCIU THEN
+        dbms_output.put_line('AVISO: CIUDADES NO COINCIDENTES');
+        RETURN;
+    WHEN OTHERS THEN
+        RETURN;
+END;
 
+SELECT * FROM EMPLOYEES;
+UPDATE EMPLOYEES SET DEPARTMENT_ID = 60 WHERE EMPLOYEE_ID = 206; --110 ORIGINAL
+
+
+SET SERVEROUTPUT ON;
 /*5. Codifica triggers de sistema para que se inserten registros de auditoría en la tabla
 que hemos creado en clase AUDITORIA_EVENTOS_BD con las siguientes
-condiciones
-    a. Cuando se desconecte el usuario SYSTEM
-    b. Cuando cree una tabla en el sistema
-    c. Cuando se asignen permisos*/
+condiciones*/
+CREATE TABLE AUDITORIA_EVENTOS_BD(
+USUARIO VARCHAR2(50),
+EVENTO VARCHAR2(50),
+FECHA DATE,
+PRIMARY KEY (USUARIO,EVENTO,FECHA)
+);
+
+CREATE OR REPLACE TRIGGER AUDITORIA_LOGIN
+    AFTER LOGON ON DATABASE
+BEGIN
+    INSERT INTO AUDITORIA_EVENTOS_BD VALUES (USER,ORA_SYSEVENT,SYSDATE);
+END;
+
+    --a. Cuando se desconecte el usuario SYSTEM
+CREATE OR REPLACE TRIGGER AUDITORIA_LOGOUT
+    BEFORE LOGOFF ON DATABASE
+BEGIN
+    IF USER = 'SYSTEM' THEN
+        INSERT INTO AUDITORIA_EVENTOS_BD VALUES (USER,ORA_SYSEVENT,SYSDATE);
+    END IF;
+END;
+
+    --b. Cuando cree una tabla en el sistema
+CREATE OR REPLACE TRIGGER AUDITORIA_CREATETABLE
+    AFTER CREATE ON DATABASE
+BEGIN
+    INSERT INTO AUDITORIA_EVENTOS_BD VALUES (USER,ORA_SYSEVENT,SYSDATE);
+END;
+
+    --c. Cuando se asignen permisos
+CREATE OR REPLACE TRIGGER AUDITORIA_GRANT
+    AFTER GRANT ON DATABASE
+BEGIN
+    INSERT INTO AUDITORIA_EVENTOS_BD VALUES (USER,ORA_SYSEVENT,SYSDATE);
+END;
 
 /*6. Crea un trigger de sistema que guarde en una tabla que debes crear el usuario, la
 fecha, el código de error de oracle (ora_server_error(1)) y el mensaje de error de
 oracle (columna varchar2(4000) y ora_server_error_msg(1)) cada vez que haya un
 error en el sistema.*/
+
+CREATE TABLE ERROR_EJECUCION(
+USUARIO VARCHAR2(50),
+COD_ERROR VARCHAR2(50),
+FECHA DATE,
+MSG_ERROR VARCHAR2(4000),
+PRIMARY KEY (USUARIO,COD_ERROR,FECHA)
+);
+
+CREATE OR REPLACE TRIGGER SYSTEM_ERROR
+    AFTER SERVERERROR ON DATABASE
+BEGIN
+    INSERT INTO ERROR_EJECUCION VALUES (USER,ora_server_error(1),SYSDATE,ora_server_error_msg(1));
+END;
